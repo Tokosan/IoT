8b3c2f3ab311e2607367e26fb2d0fc6821028fa1
Tokosan <cteiba.al@gmail.com>
2024-06-26 15:08:36 -0400

    rasp

--- Tareas/2/rasp/ble_test.py
+++ Tareas/2/rasp/ble_test.py
@@ -1,21 +1,29 @@
 import asyncio
 from bleak import BleakClient, BleakScanner
+from parser import parse_packet
 
 CHARACTERISTIC_UUID = "0000FF01-0000-1000-8000-00805F9B34FB"
+ADDRESS = "C0:49:EF:08:D3:AE"
 
-async def scan():
-    # Con esto podemos ver los dispositivos que estan disponibles
-    scanner = BleakScanner()
-    devices = await scanner.discover()
-    print("Devices:")
-    # c0:49:ef:08:d3:ae 
-    print(devices)
-    print("+-------MAC-------+--------------------------+")
-    esps = []
-    for device in devices:
-        if "ESP" in device.name:
-            esps.append(device.address)
-    return esps
+async def get_client():
+    client = BleakClient(ADDRESS)
+    print("Trying to connect:", client)
+    i = 0
+    while True:
+        try:
+            connected = await client.connect()
+            break
+        except:
+            i+=1
+    print(f"Connected (after {i} tries)")
+    data = await client.read_gatt_char(CHARACTERISTIC_UUID)
+    print(parse_packet(data))
+    return client
 
-esps = asyncio.run(scan())
-print("ESPS:", esps)
+client = asyncio.run(get_client())
+
+# async def get_data(client):
+    
+# print("now getting data")
+
+# asyncio.run(get_data(client))
--- Tareas/2/rasp/main_ble.py
+++ Tareas/2/rasp/main_ble.py
@@ -56,26 +56,4 @@ async def main():
     packet = parse_packet(data)
     print(packet)
     
-
 asyncio.run(main())
-
-quit()
-
-def convert_to_128bit_uuid(short_uuid):
-    # Usada para convertir un UUID de 16 bits a 128 bits
-    # Los bits fijos son utilizados para BLE ya que todos los UUID de BLE son de 128 bits
-    # y tiene este formato: 0000XXXX-0000-1000-8000-00805F9B34FB
-    base_uuid = "0000ff01-0000-1000-8000-00805F9B34FB"
-    short_uuid_hex = "{:04X}".format(short_uuid)
-    return base_uuid[:4] + short_uuid_hex + base_uuid[8:]
-
-
-# CHARACTERISTIC_UUID = convert_to_128bit_uuid(0xFF01) # Busquen este valor en el codigo de ejemplo de esp-idf
-
-async def main(ADDRESS):
-    async with BleakClient(ADDRESS) as client:
-        data = await client.read_gatt_char(CHARACTERISTIC_UUID)
-        print("Characterisic A {0}".format("".join(map(chr, data))))
-        # Luego podemos escribir en la caracteristica
-        data = 'Hello World!'.encode()
-        await client.write_gatt_char(CHARACTERISTIC_UUID, b"\x01\x00")
